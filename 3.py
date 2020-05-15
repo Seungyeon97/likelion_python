c1a5b56864e8d8857871dd7288ebd64c19d1c2da
Seungyeon97 <xxyeonny@likelion.org>
2020-05-16 04:04:28 +0900

    3번문제

---
+++ 3.py
@@ -0,0 +1,7 @@
+register_number = "881120-1068234"
+year = register_number[0:2]
+month = register_number[2:4]
+day = register_number[4:6]
+number = register_number[7:]
+
+print(year,month, day, number)
